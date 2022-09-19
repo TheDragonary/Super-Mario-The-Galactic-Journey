a263540f0139335e4eed223666b4a16373273796
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2022-09-19 18:31:07 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,8 +10,15 @@ gLevelValues.entryLevel = LEVEL_CASTLE_GROUNDS
 gLevelValues.exitCastleLevel = LEVEL_CASTLE_GROUNDS
 gLevelValues.exitCastleWarpNode = 0x0A
 gLevelValues.skipCreditsAt = LEVEL_CASTLE_GROUNDS
+gLevelValues.coinsRequiredForCoinStar = -1
 
 ------------
 -- camera --
 ------------
 camera_set_use_course_specific_settings(false)
+
+-- No Fall Damage mod made by Mr.Needlemouse64
+function no_fall_damage(m)
+ m.peakHeight = m.pos.y
+end
+hook_event(HOOK_MARIO_UPDATE, no_fall_damage)
